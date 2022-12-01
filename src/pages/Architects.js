17544f8b59794d883a6d146e1ee72c1c03e6b184
Alexei Bobrik <fingerloom2004@gmail.com>
2022-12-01 14:57:21 +0300

    feat: create architects page

--- src/pages/Architects.js
+++ src/pages/Architects.js
@@ -2,23 +2,32 @@ import './Architects.css'
 import {Form} from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { useTranslation } from 'react-i18next';
+import data from "../noLangData.json";
+import {Link} from "react-router-dom";
 
 function Architects() {
+    const { t, i18n } = useTranslation();
     return (
         <>
             <div className="architects-section">
                 <div className="arch-wrapper">
-                    <h1 className="arch-list-heading">Список архитекторов Беларуси</h1>
+                    <h1 className="arch-list-heading">{t("architectsListHeader")}</h1>
                     <Form className="search-form">
-                        <Form.Control className="search-box" type="text" placeholder="Поиск архитектора" />
+                            <Form.Control className="search-box" type="text" placeholder={t("searchCaption")} />
                     </Form>
                 </div>
 
-                <Card>
+                <Card className="architects-list">
                     <ListGroup variant="flush">
-                        <ListGroup.Item>Ананич Юрий Отчество</ListGroup.Item>
-                        <ListGroup.Item>Очередной архитектор</ListGroup.Item>
-                        <ListGroup.Item>И ещё один</ListGroup.Item>
+                        {Object.keys(data).map((id, index) =>
+                            <ListGroup.Item action as={Link} to={`/architects/${id}`}>
+                                <p className="arch-fio">{t(`architects.${id}.surname`)} {t(`architects.${id}.name`)} {t(`architects.${id}.patronymic`)}</p>
+                                <p className="arch-descr">{t(`architects.${id}.shortDescription`)}</p>
+                            </ListGroup.Item>
+
+                        )}
+
                     </ListGroup>
                 </Card>
 
